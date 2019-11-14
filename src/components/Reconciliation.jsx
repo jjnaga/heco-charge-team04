c337158e4d789f709db5cd769b7862ac9d74f264
Damien <dlim7@hawaii.edu>
2019-11-13 21:51:08 -1000

    added form to Reconciliation component

--- src/components/Reconciliation.jsx
+++ src/components/Reconciliation.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled from "styled-components";
-import { Popup, Progress, Header, Button, Item } from "semantic-ui-react";
+import { Popup, Progress, Header, Button, Item, Form, Checkbox } from "semantic-ui-react";
 import PropTypes from 'prop-types';
 import { useQuery } from "urql";
 
@@ -24,8 +24,10 @@ const getReconcileData = `
   }
 `;
 
+
 const Reconcilliation = ({ toggle }) => {
   const [{ fetching, error, data }] = useQuery({ query: getReconcileData });
+
   if (fetching) return "Fetching";
   else if (error) {
     console.log(error);
@@ -39,29 +41,62 @@ const Reconcilliation = ({ toggle }) => {
       <Container className="reconcile" >
         <Item.Group divided>
           {reconcileData.map(row => {
-            let { station, duration, starttime, endtime, energy, id } = row;
+            let { station, duration, starttime, endtime, energy, id, amount } = row;
             starttime = new Date(starttime);
-            console.log(starttime);
-            console.log(typeof starttime);
+            
+            //update mutation
+            const mutateReconcileData = `
+              mutation updateReconcile {
+                _typename
+                update_chargedata_reconcile(where: {id: {_eq: \"${id}\"}}, 
+                _set: {amount: "\"${amount}\"", duration"\"${duration}\"", energy"\"${energy}\""}) {
+                    returning {
+                      \"${id}\"
+                    }
+                }
+              }
+            `;
+            //on submission mutate the db
+            const handleSubmit = () => {
+              //const [{ fetching, error, data }] = useQuery({ query: mutateReconcileData });
+              alert(`id:\"${id}\" submitted`);
+            }
+            //on change update props
+            const handleChange = () => {
+              
+            }
             return (
               <Item>
                 <Item.Content>
                   <Item.Header as='a'>{station}</Item.Header>
                   <Item.Meta>{`${starttime.toLocaleString()}`}</Item.Meta>
-                  <Item.Content>{energy}</Item.Content>
+                  <Item.Content>id:{id}</Item.Content>
                 </Item.Content>
+                <Form>
+                <Form.Field>
+                  <label>Energy = {energy}</label>
+                  <input placeholder='Energy' />
+                </Form.Field>
+                <Form.Field>
+                  <label>Duration = {duration}</label>
+                  <input placeholder='Duration' />
+                </Form.Field>
+                <Form.Field>
+                  <label>Amount = {amount}</label>
+                  <input placeholder='Amount' />
+                </Form.Field>
+                <button onClick={handleSubmit}>Submit</button>
+              </Form>
               </Item>
+              
             )
           })}
         </Item.Group>
-        <div>
-          table
-        </div>
       </Container>
     )
   }
 }
-
+//
 // const ExitContainer = styled.div`
 //   margin-left: auto;
 // `;
